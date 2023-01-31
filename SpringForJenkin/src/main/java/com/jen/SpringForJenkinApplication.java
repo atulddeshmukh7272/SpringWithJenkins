e36873667f05c586b0d6ecfae08086b4da80986b
atuld <atul>
2023-01-31 20:10:05 +0530

    1st commit

---
+++ SpringForJenkin/src/main/java/com/jen/SpringForJenkinApplication.java
@@ -0,0 +1,13 @@
+package com.jen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringForJenkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringForJenkinApplication.class, args);
+	}
+
+}
