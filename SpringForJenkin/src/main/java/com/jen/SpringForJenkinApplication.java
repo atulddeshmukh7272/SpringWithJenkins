95b39db9cc4275f45389aa400edb27bdff13afe8
atulddeshmukh7272 <121999134+atulddeshmukh7272@users.noreply.github.com>
2023-02-01 10:18:43 +0530

    4rh commite

--- SpringForJenkin/src/main/java/com/jen/SpringForJenkinApplication.java
+++ SpringForJenkin/src/main/java/com/jen/SpringForJenkinApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringForJenkinApplication {
 
 	public static void main(String[] args) {
-		
 		SpringApplication.run(SpringForJenkinApplication.class, args);
 	}
 
